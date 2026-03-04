c54342aad3df932b578d5a3589f77db961367525
gennadii-roud <gennady@roudstudio.com>
2026-03-04 18:21:23 +0200

    change config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,45 @@
+<script lang="ts">
+	let firstName = "";
+	let lastName = "";
+	let result: string | null = null;
+
+	async function handleSubmit() {
+		const formData = new FormData();
+		formData.append("firstName", firstName);
+		formData.append("lastName", lastName);
+
+		try {
+			const resp = await fetch("/contact", {
+				method: "POST",
+				body: formData
+			});
+
+			result = await resp.text();
+		} catch (err) {
+			result = "Server error";
+		}
+	}
+</script>
+
 <div style="display: flex; justify-content: center; align-items: center; min-width: 100%; min-height: 100%; background-color: navajowhite; position: absolute; inset: 0">
-	<form method="POST" action="/contact" style="display: flex; flex-direction: column; gap: 3rem">
+	<form
+		on:submit|preventDefault={handleSubmit}
+		style="display: flex; flex-direction: column; gap: 3rem"
+	>
 		<label>
 			First Name:
-			<input type="text" name="firstName" required />
+			<input type="text" bind:value={firstName} required />
 		</label>
 
 		<label>
 			Last Name:
-			<input type="text" name="lastName" required />
+			<input type="text" bind:value={lastName} required />
 		</label>
 
 		<button type="submit">Submit</button>
 	</form>
+
+	{#if result}
+		<p>{result}</p>
+	{/if}
 </div>
